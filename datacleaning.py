6bca502e7c4e68675a6aab74d00749d60a2f0e3f
isabelli31 <isabellicristhini@gmail.com>
2023-10-09 15:35:02 -0300

    func filtrar_coluna_com_termo

--- datacleaning.py
+++ datacleaning.py
@@ -14,3 +14,21 @@ def coletar_colunas(df, colunas_desejadas: list):
     """
     df = df[[colunas_desejadas]]
     return df
+
+def filtrar_coluna_com_termo(df, coluna_a_ser_filtrada: str, topico_desejado: str):
+    """filtrar_coluna_com_termo
+
+    #(descricao da funcao)#
+
+    Parameters:
+        df (dataframe): dataframe original
+        coluna_a_ser_filtrada (str): _description_
+        topico_desejado (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    filtro = df['coluna_a_ser_filtrada'].str.contains(topico_desejado, case=False)
+    df = df[filtro]
+    
+    return df

d5fe25ad3655c55b586f6ac06b0c34b7d10e131a
nataliabrandão <nataliabrandaos55@gmail.com>
2023-10-11 19:03:53 -0300

    boxplot e upload dos arquivos limpos (datacleaning)

---
+++ analiseexporatoria.py
@@ -0,0 +1,41 @@
+import pandas as pd 
+import datacleaning as dtc
+
+df1 = pd.read_csv('data/2014_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df2 = pd.read_csv('data/2015_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df3 = pd.read_csv('data/2016_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df4 = pd.read_csv('data/2017_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df5 = pd.read_csv('data/2018_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df6 = pd.read_csv('data/2019_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df7 = pd.read_csv('data/2020_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df8 = pd.read_csv('data/2021_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df9 = pd.read_csv('data/2022_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df10 = pd.read_csv('data/2023_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+
+df = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8, df9, df10], axis=0)
+
+print(type(df))
+
+df.reset_index(drop=True, inplace=True)
+
+print(df.head())
+print(df.tail())
+
+print(df.columns.values)
+
+#conversão dos valores na coluna de orçamentos para float
+df['ORÇAMENTO REALIZADO (R$)'] = pd.to_numeric(df['ORÇAMENTO REALIZADO (R$)'], errors='coerce')
+
+#limpeza dos dados
+df_final = pd.DataFrame()
+df_orcamentos_negativos = pd.DataFrame()
+df_orcamentos_positivos = pd.DataFrame()
+
+df_final = dtc.coletar_colunas(df, ["EXERCÍCIO", "NOME FUNÇÃO", 'NOME SUBFUNÇÃO','ORÇAMENTO REALIZADO (R$)'])
+df_final = dtc.filtrar_coluna_com_termo(df_final, "NOME FUNÇÃO", "educação")
+df_orcamentos_positivos = dtc.filtrar_colunas_numericas(df_final,"ORÇAMENTO REALIZADO (R$)", 1)
+df_orcamentos_negativos = dtc.filtrar_colunas_numericas(df_final,"ORÇAMENTO REALIZADO (R$)", 0)
+dtc.valores_invalidos(df_final)
+
+#arquivo csv limpo
+df_final.to_csv('data/data_final.csv', index="false")
---
+++ boxplot.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import analiseexporatoria as ae
+
+ae.df_final.plot(kind='box',figsize =(10,6), subplots = True)
+
+

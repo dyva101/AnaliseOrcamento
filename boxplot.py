eb0ae1d12cdd802f00fb83aeffd23ba087907105
nataliabrandão <nataliabrandaos55@gmail.com>
2023-10-11 19:28:45 -0300

    boxplot

--- boxplot.py
+++ boxplot.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import analiseexporatoria as ae
+import matplotlib.pyplot as plt
 
-ae.df_final.plot(kind='box',figsize =(10,6), subplots = True)
+# Selecionar apenas as colunas numéricas do DataFrame
+numeric_columns = ae.df_final.select_dtypes(include=['number'])
 
+fig, axes = plt.subplots(figsize=(10, 6), nrows=1, ncols=len(numeric_columns.columns))
 
+for i, column in enumerate(numeric_columns.columns):
+    numeric_columns[column].plot(kind='box', ax=axes[i])
+
+plt.show()

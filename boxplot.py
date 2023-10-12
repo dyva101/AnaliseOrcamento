f849c9f4cb53b1496340dde68249f73702100276
nataliabrandão <nataliabrandaos55@gmail.com>
2023-10-12 20:33:23 -0300

    docstring da funcao separar_outliers_colunas_numericas

--- boxplot.py
+++ boxplot.py
@@ -9,6 +9,17 @@ def boxplot_coluna_de_dataframe(df, coluna):
 
     plt.show()
 
+    """
+    Cria um gráfico de boxplot para uma coluna específica de um DataFrame.
+
+    Parameters:
+        df (pandas.DataFrame): O DataFrame contendo os dados.
+        coluna (str): O nome da coluna a ser visualizada no gráfico de boxplot.
+
+    Returns:
+        None
+    """
+
 def separar_outliers_colunas_numericas(df, coluna_numerica):
     
     data = df[coluna_numerica]
@@ -19,6 +30,17 @@ def separar_outliers_colunas_numericas(df, coluna_numerica):
     df_com_outliers = df[(df[coluna_numerica] < limite_inferior) | (df[coluna_numerica] > limite_superior)]
 
     return df_com_outliers
+    
+    """
+    Separa os outliers de uma coluna numérica de um DataFrame com base em um boxplot.
+
+    Parameters:
+        df (pandas.DataFrame): O DataFrame contendo os dados.
+        coluna_numerica (str): O nome da coluna numérica que deseja separar os outliers.
+
+    Returns:
+        pandas.DataFrame: Um novo DataFrame contendo apenas as linhas que correspondem aos outliers da coluna numérica.
+    """
 
 def criar_dataframe_sem_outliers(df, coluna_numerica):
     data = df[coluna_numerica]

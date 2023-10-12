b43f0e954c6cea1972137941013a74c9295e4246
nataliabrandão <nataliabrandaos55@gmail.com>
2023-10-12 11:50:34 -0300

    ajuste coluna

--- boxplot.py
+++ boxplot.py
@@ -1,13 +1,10 @@
 import pandas as pd
+import datacleaning as dtc
 import analiseexporatoria as ae
 import matplotlib.pyplot as plt
 
-# Selecionar apenas as colunas numéricas do DataFrame
-numeric_columns = ae.df_final.select_dtypes(include=['number'])
+# Certifique-se de que as colunas que você deseja converter para numérico estão corretas
+ae.df_final['NOME FUNÇÃO'] = pd.to_numeric(ae.df_final['NOME FUNÇÃO'], errors='coerce')
+ae.df_final['NOME SUBFUNÇÃO'] = pd.to_numeric(ae.df_final['NOME SUBFUNÇÃO'], errors='coerce')
+ae.df_final['EXERCÍCIO'] = pd.to_numeric(ae.df_final['EXERCÍCIO'], errors='coerce')
 
-fig, axes = plt.subplots(figsize=(10, 6), nrows=1, ncols=len(numeric_columns.columns))
-
-for i, column in enumerate(numeric_columns.columns):
-    numeric_columns[column].plot(kind='box', ax=axes[i])
-
-plt.show()

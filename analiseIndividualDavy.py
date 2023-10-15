60e7b0ae824915e91acac939cf1ec0c5e37c1493
dyva101 <davyuser456@gmail.com>
2023-10-15 05:19:34 -0300

    analise individual davy: pandemia e anos eleitorais

--- analiseIndividualDavy.py
+++ analiseIndividualDavy.py
@@ -1,53 +1,92 @@
 # Import necessary modules
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import boxplot as bx
 import datacleaning as dtc
 import graficos as grf
 import analiseexporatoria as ae
 
-df_geral = pd.read_csv("data/")
+df1 = pd.read_csv('data/2014_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df2 = pd.read_csv('data/2015_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df3 = pd.read_csv('data/2016_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df4 = pd.read_csv('data/2017_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df5 = pd.read_csv('data/2018_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df6 = pd.read_csv('data/2019_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df7 = pd.read_csv('data/2020_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df8 = pd.read_csv('data/2021_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df9 = pd.read_csv('data/2022_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+df10 = pd.read_csv('data/2023_OrcamentoDespesa.csv', encoding='windows-1252', delimiter=';')
+
+df = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8, df9, df10], axis=0)
+
+df.reset_index(drop=True, inplace=True)
+
+#conversão dos valores na coluna de orçamentos para float
+df['ORÇAMENTO REALIZADO (R$)'] = df['ORÇAMENTO REALIZADO (R$)'].str.replace(',', '.').astype(float)
+
+#limpeza dos dados
+df_final = pd.DataFrame()
+df_orcamentos_negativos = pd.DataFrame()
+df_orcamentos_positivos = pd.DataFrame()
+
+df_final = dtc.coletar_colunas(df, ["EXERCÍCIO", "NOME FUNÇÃO", 'NOME SUBFUNÇÃO','ORÇAMENTO REALIZADO (R$)'])
+df_final = dtc.filtrar_coluna_com_termo(df_final, "NOME FUNÇÃO", "educação")
 
 ### Boxplot E Estatísticas para análise dos outliers ###
 
-# Remove outliers from the data
-df_sem_outliers = ae.df_sem_outliers
+# Plot BoxPlots of the Data
+bx.boxplot_coluna_de_dataframe(df_final, 'ORÇAMENTO REALIZADO (R$)')
+bx.boxplot_sem_outliers(df_final, 'ORÇAMENTO REALIZADO (R$)', 'sem_outliers')
 
-# Plot a boxplot of the data
-plot_boxplot(df_sem_outliers)
-
-# Plot a histogram of the data
-plot_histogram(df_sem_outliers)
-
-# Calculate some statistics of the data
-mean = df_sem_outliers.mean()
-median = df_sem_outliers.median()
-std = df_sem_outliers.std()
+# Remove the outliers
+df_sem_outliers = bx.excluir_outliers(df_final, 'ORÇAMENTO REALIZADO (R$)')
 
 # Print the statistics
-print(f"Mean: {mean}")
-print(f"Median: {median}")
-print(f"Standard Deviation: {std}")
+print(df_sem_outliers.describe())
 
 ### Análise de correlação entre o ano eleitoral e os outros anos ###
 
-    # Calculate the mean for the non-election years
-    # Calculate the mean for the election years
-    # Compare the difference in means and show if it does conffirms the hypothesis
+# Calculate the mean for the non-election years
+
+# Calculate the mean for the election years
+
+# Compare the difference in means and show if it does conffirms the hypothesis
 
 ### Análise da queda brusca de investimento no período pandêmico (2019-2021) ###
 
-    # Calculate the mean for the pre-pandemic years
-    # Calculate the mean for the pandemic years
-    # Hypothesis: the mean for the pandemic years is lower than the mean for the pre-pandemic years
-        # Plot a stacked bar chart of the 10 year period 2014-2023
-        # Plot histograms of 2020 and 2021
-    # Use all the intel gathered to confirm or reject the hypothesis
+# Calculate the mean for the pre-pandemic years
+orcamentos_anos_pre_pandemia = []
+for ano in [2014, 2015, 2016, 2017, 2018]:
+    dtc.filtrar_coluna_com_termo(df_geral, "EXERCÍCIO", ano)
+    orcamentos_anos_pre_pandemia.append(df_geral["ORÇAMENTO REALIZADO (R$)"].sum())
+    
+print(orcamentos_anos_pre_pandemia.mean())
+
+# Calculate the mean for the pandemic years
+orcamentos_anos_pos_pandemia = []
+for ano in [2019, 2020, 2021]:
+    dtc.filtrar_coluna_com_termo(df_geral, "EXERCÍCIO", ano)
+    orcamentos_anos_pos_pandemia.append(df_geral["ORÇAMENTO REALIZADO (R$)"].sum())
+    
+print(orcamentos_anos_pos_pandemia.mean())
+    
+# Hypothesis: the expenditure in education diminished in the pandemic years, for all sectors
+
+# Plot a stacked bar chart of the 10 year period 2014-2023
+grf.plotar_colunas_empilhadas(df_geral, "EXERCÍCIO", "ORÇAMENTO REALIZADO (R$)", "NOME FUNÇÃO", "Gastos por função", 2014, 2023)
+
+# Plot a histogram of the data
+for ano in [2020, 2021]:
+    grf.plotar_histograma_com_filtro(df_sem_outliers, "ORÇAMENTO REALIZADO (R$)", 'EXERCÍCIO', ano, f'Gastos em {ano}' )
+
+# Use all the intel gathered to confirm or reject the hypothesis
 
 ### Rankings: Governos com maior investimento bruto em educação e seus subcampos ###
 
-    #Ranking com os 3 mandadtos do período, em ordem de investimento bruto em educação (stacked bar chart)
-    #Ranking com os 3 mandatos do período, em ordem de investimento bruto em educação básica (stacked bar chart)
-    #Ranking com os 3 mandatos do período, em ordem de investimento bruto em educação superior (stacked bar chart)
-    #Ranking com os 3 mandatos do período, em ordem de investimento bruto em educação profissional (stacked bar chart)
+#Ranking com os 3 mandadtos do período, em ordem de investimento bruto em educação (stacked bar chart)
+
+#Ranking com os 3 mandatos do período, em ordem de investimento bruto em educação básica (stacked bar chart)
+
+#Ranking com os 3 mandatos do período, em ordem de investimento bruto em educação superior (stacked bar chart)
+
+#Ranking com os 3 mandatos do período, em ordem de investimento bruto em educação profissional (stacked bar chart)

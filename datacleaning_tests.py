501745139481e545a721752b8b3c8459568f6154
dyva101 <davyuser456@gmail.com>
2023-10-15 23:39:33 -0300

    testecoletarcolunas unittest ajustes done

--- datacleaning_tests.py
+++ datacleaning_tests.py
@@ -8,21 +8,44 @@ import graficos as grf
 
 class TesteColetarColunas(un.TestCase):
     
-    def test_colunas_desejadas_validas(self):
-        dataframe = {'A': [1, 999, 31], 'B': ['EMAp', 'força', 'ficar'], 'C': [8.7, 7.5, 3.7]}
+    def test_valid_dataframe_and_columns(self):
+        dataframe = {'A': [1,999,31], 'B': ['EMAp', 'força', 'ficar'], 'C': [8.7, 7.5, 3.7]}
         df = pd.DataFrame(dataframe)
+        result = dtc.coletar_colunas(df, ['A', 'B'])
+        expected = pd.DataFrame({'A': [1, 999, 31], 'B': ['EMAp', 'força', 'ficar']})
+        assert result.equals(expected)
 
-        # Teste para colunas 'A' e 'B'
-        colunas_desejadas = ['A', 'B']
-        resultado = dtc.coletar_colunas(df, colunas_desejadas)
-        expected_df = pd.DataFrame({'A': [1, 999, 31], 'B': ['EMAp', 'força', 'ficar']})
-        self.assertTrue(resultado.equals(expected_df))
+    def test_empty_list_of_columns(self):
+        dataframe = {'A': [1,999,31], 'B': ['EMAp', 'força', 'ficar'], 'C': [8.7, 7.5, 3.7]}
+        df = pd.DataFrame(dataframe)
+        result = dtc.coletar_colunas(df, [])
+        expected = pd.DataFrame()
+        assert result.equals(expected)
+    
+    def test_non_list_object_as_second_argument(self):
+        dataframe = {'A': [1,999,31], 'B': ['EMAp', 'força', 'ficar'], 'C': [8.7, 7.5, 3.7]}
+        df = pd.DataFrame(dataframe)
+        with un.ytest.raises(TypeError):
+            dtc.coletar_colunas(df, 2) 
+    
+    def test_non_dataframe_object_as_first_argument(self):
+        with un.pytest.raises(TypeError):
+            dtc.coletar_colunas("osmar", ['A','B'])   
 
-        # Teste para colunas 'A' e 'C'
-        colunas_desejadas = ['A', 'C']
-        resultado = dtc.coletar_colunas(df, colunas_desejadas)
-        expected_df = pd.DataFrame({'A': [1, 999, 31], 'C': [8.7, 7.5, 3.7]})
-        self.assertTrue(resultado.equals(expected_df))
+    def test_column_not_belonging_to_dataframe(self):
+        dataframe = {'A': [1,999,31], 'B': ['EMAp', 'força', 'ficar'], 'C': [8.7, 7.5, 3.7]}
+        df = pd.DataFrame(dataframe)
+        with un.pytest.raises(KeyError):
+            dtc.coletar_colunascoletar_colunas(df, ['A','F'])
 
+    def test_no_list_of_columns_passed(self):
+        dataframe = {'A': [1,999,31], 'B': ['EMAp', 'força', 'ficar'], 'C': [8.7, 7.5, 3.7]}
+        df = pd.DataFrame(dataframe)
+        result = dtc.coletar_colunas(df, ['A', 'B', 'C'])
+        expected = pd.DataFrame({'A': [1, 999, 31], 'B': ['EMAp', 'força', 'ficar'], 'C': [8.7, 7.5, 3.7]})
+        assert result.equals(expected)
 
+class TesteFiltrarColunaComTermo(un.TestCase):
+    
+    
 #class TestePlotarColunasEmpilhadas(un.TestCase):

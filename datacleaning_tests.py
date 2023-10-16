228a21a77efe20765f01b66f92adedf5b5618781
dyva101 <davyuser456@gmail.com>
2023-10-16 00:16:14 -0300

    testefiltrarcolunaportermo  unittests done

--- datacleaning_tests.py
+++ datacleaning_tests.py
@@ -47,5 +47,39 @@ class TesteColetarColunas(un.TestCase):
 
 class TesteFiltrarColunaComTermo(un.TestCase):
     
+    def test_filtered_dataframe_with_desired_term(self):
+        dataframe = {'letras do alfabeto': ['a', 'b','c', 'd' ], 'forma maiúscula': ['A', 'B', 'C', 'D']}
+        df = pd.DataFrame(dataframe)
+        filtered_df = dtc.filtrar_coluna_com_termo(df, 'letras do alfabeto', 'a')
+        assert filtered_df.equals(pd.DataFrame({'letras do alfabeto': ['a'], 'forma maiúscula': ['A']}))
+        
+    def test_empty_dataframe_if_no_rows_contain_desired_term(self):
+        dataframe = {'letras do alfabeto': ['a', 'b','c', 'd' ], 'forma maiúscula': ['A', 'B', 'C', 'D']}
+        df = pd.DataFrame(dataframe)
+        filtered_df = dtc.filtrar_coluna_com_termo(df, 'letras do alfabeto', 'z')
+        assert filtered_df.empty
     
+    def test_case_insensitive(self):
+        dataframe = {'letras do alfabeto': ['a', 'b','c', 'd' ], 'forma maiúscula': ['A', 'B', 'C', 'D']}
+        df = pd.DataFrame(dataframe)
+        filtered_df = dtc.filtrar_coluna_com_termo(df, 'letras do alfabeto', 'A')
+        assert filtered_df.equals(pd.DataFrame({'letras do alfabeto': ['a'], 'forma maiúscula': ['A']}))       
+    
+    def test_input_dataframe_not_pandas_dataframe(self):
+        with un.pytest.raises(TypeError):
+            dtc.filtrar_coluna_com_termo("almir", 'forma maiúscula', 'c')
+            
+    def test_input_column_name_not_string(self):
+        dataframe = {'letras do alfabeto': ['a', 'b','c', 'd' ], 'forma maiúscula': ['A', 'B', 'C', 'D']}
+        df = pd.DataFrame(dataframe)
+        with un.pytest.raises(TypeError):
+            dtc.filtrar_coluna_com_termo(df, 23, 'A')
+
+    def test_input_column_name_not_exist(self):
+        dataframe = {'letras do alfabeto': ['a', 'b','c', 'd' ], 'forma maiúscula': ['A', 'B', 'C', 'D']}
+        df = pd.DataFrame(dataframe)
+        with un.pytest.raises(KeyError):
+            dtc.filtrar_coluna_com_termo(df, 'forma numérica', 'a')
+
+class Teste
 #class TestePlotarColunasEmpilhadas(un.TestCase):
